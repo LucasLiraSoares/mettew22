37cbbb5ac36412a6eb71bf182e86561c269e4698
LucasLiraSoares <148035705+LucasLiraSoares@users.noreply.github.com>
2025-05-12 04:31:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// Bolinha seguindo o mouse
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', e => {
+    cursor.style.top = e.clientY + 'px';
+    cursor.style.left = e.clientX + 'px';
+});
